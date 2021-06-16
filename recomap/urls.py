9079d44ba7fee35ac27e8c533d836b67591a0c3d
NakMits <mitsuru-miyamoto@recomap.com>
2021-06-17 00:25:24 +0900

    Add index page

--- recomap/urls.py
+++ recomap/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'recomap'
 urlpatterns = [
-    path('', views.HelloWorldView.as_view(), name='helloworld'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('helloworld', views.HelloWorldView.as_view(), name='helloworld'),
     path('map', views.MapView.as_view(), name='map'),
 ]
--- recomap/views.py
+++ recomap/views.py
@@ -5,6 +5,9 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+class IndexView(TemplateView):
+    template_name = 'recomap/index.html'
+
 class HelloWorldView(TemplateView):
     template_name = 'recomap/helloworld.html'
 

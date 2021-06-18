70023780c44e6db3796ea27b54f712e1db5abd7e
NakMits <mitsuru-miyamoto@recomap.com>
2021-06-18 23:25:02 +0900

    Modify urls.py /

--- recomap/urls.py
+++ recomap/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('helloworld/', views.HelloWorldView.as_view(), name='helloworld'),
     path('map/', views.MapView.as_view(), name='map'),
     path('marker/', views.MarkerReadListView.as_view(), name='marker_read_list'),
-    path('marker/detail/<int:pk>', views.MarkerReadDetailView.as_view(), name='marker_read_detail'),
-    path('marker/delete/<int:pk>', views.MarkerDeleteView.as_view(), name='marker_delete'),
-    path('marker/update/<int:pk>', views.MarkerUpdateView.as_view(), name='marker_update'),
-    path('marker/create', views.MarkerCreateView.as_view(), name='marker_create'),
+    path('marker/detail/<int:pk>/', views.MarkerReadDetailView.as_view(), name='marker_read_detail'),
+    path('marker/delete/<int:pk>/', views.MarkerDeleteView.as_view(), name='marker_delete'),
+    path('marker/update/<int:pk>/', views.MarkerUpdateView.as_view(), name='marker_update'),
+    path('marker/create/', views.MarkerCreateView.as_view(), name='marker_create'),
 ]

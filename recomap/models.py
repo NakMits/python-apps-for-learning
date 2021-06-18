670058b1ed6b725934b172e5eab016644ae73751
NakMits <mitsuru-miyamoto@recomap.com>
2021-06-18 13:59:58 +0900

    Add CRUD sample page

--- recomap/models.py
+++ recomap/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MarkerModel(models.Model):
+
+    title = models.CharField(max_length=100)
+    tag = models.CharField(max_length=20)
+    memo = models.TextField()
+    lat = models.CharField(max_length=30)
+    lng = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
--- recomap/views.py
+++ recomap/views.py
@@ -1,15 +1,23 @@
 import os
 
+from django.urls import reverse_lazy
+
+from . import models
+
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, DeleteView, UpdateView
+
 
 # Create your views here.
+
 
 class IndexView(TemplateView):
     template_name = 'recomap/index.html'
 
+
 class HelloWorldView(TemplateView):
     template_name = 'recomap/helloworld.html'
+
 
 class MapView(TemplateView):
     template_name = 'recomap/map.html'
@@ -18,3 +26,35 @@ class MapView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["GOOGLEMAPS_APIKEY"] = os.environ['GOOGLEMAPS_APIKEY']
         return context
+
+
+class MarkerReadListView(ListView):
+    template_name = 'recomap/marker_read_list.html'
+    model = models.MarkerModel
+
+
+class MarkerReadDetailView(DetailView):
+    template_name = 'recomap/marker_read_detail.html'
+    model = models.MarkerModel
+
+
+class MarkerCreateView(CreateView):
+    template_name = 'recomap/marker_create.html'
+    model = models.MarkerModel
+    fields = ('title', 'tag', 'memo', 'lat', 'lng')
+    success_url = reverse_lazy('recomap:marker_read_list')
+
+
+class MarkerDeleteView(DeleteView):
+    template_name = 'recomap/marker_delete.html'
+    model = models.MarkerModel
+    success_url = reverse_lazy('recomap:marker_read_list')
+
+
+class MarkerUpdateView(UpdateView):
+    template_name = 'recomap/marker_update.html'
+    model = models.MarkerModel
+    fields = ('title', 'tag', 'memo', 'lat', 'lng')
+    success_url = reverse_lazy('recomap:marker_read_list')
+
+
